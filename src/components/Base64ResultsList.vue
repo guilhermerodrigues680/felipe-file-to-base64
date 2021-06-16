827c3c73f80f9956a1abc202cbaf8141d6622799
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2021-06-16 18:01:21 -0300

    FIX: Dimensões da imagem de pre visualizaçao

--- src/components/Base64ResultsList.vue
+++ src/components/Base64ResultsList.vue
@@ -3,7 +3,7 @@
     <div v-for="(result, idx) in base64Results" :key="idx" class="flex gap-x-2">
       <span class="self-center">({{idx+1}})</span>
       <a :href="result" target="_blank" class="text-blue-500 underline">
-        <img class="border border-gray-400 p-1 rounded" :src="result" :alt="`img-idx-${idx}`">
+        <img class="border border-gray-400 p-1 rounded max-h-48 max-w-xs" :src="result" :alt="`img-idx-${idx}`">
       </a>
       <textarea :value="result" disabled class="w-full p-2 font-light overflow-x-scroll whitespace-pre resize-none bg-green-900 text-white">
       </textarea>
